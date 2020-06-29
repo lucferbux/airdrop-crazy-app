43f1234ee3bc76ea44b1e32886010418b1317ae0
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2020-06-29 12:30:04 +0200

    updated scene view

--- Airdrop Tool/SceneDelegate.swift
+++ Airdrop Tool/SceneDelegate.swift
@@ -41,7 +41,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        print("entramos aqui")
 //        ResultViewModel.sharedInstance.updateSession()
        
     }
